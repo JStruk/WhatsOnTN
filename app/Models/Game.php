6134761cf1f602ba21fd8394bcc8a68c726eb986
Justin Struk <10167488+JStruk@users.noreply.github.com>
2025-12-03 11:05:54 -0500

    add Game model and migration

---
+++ app/Models/Game.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'league',
+        'external_id',
+        'game_date',
+        'start_time_utc',
+        'status',
+        'venue',
+        'venue_timezone',
+        'home_team',
+        'away_team',
+        'home_score',
+        'away_score',
+        'link',
+    ];
+
+    protected $casts = [
+        'game_date' => 'date',
+        'start_time_utc' => 'datetime',
+    ];
+}
+
+
